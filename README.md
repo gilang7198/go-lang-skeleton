4ba217ff316fd6c2f04383d63169b94e9ccef1ad
gilang7198 <gilang7198@gmail.com>
2019-10-02 16:34:03 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# go-lang-skeleton
+
+go-lang-skeleton is an API for learning. It consists of three main service
+
+### Tech
+
+go-lang-skeleton uses a number of open source projects to work properly:
+
+* Mux
+* Gorm
+* JWT
+* .ENV
+
+### Installation
+
+go-lang-skeleton requires go version go1.12.7 to run.
+
+Install the dependencies and devDependencies and start the server.
+
+```sh
+$ go get github.com/gorilla/mux
+$ go get github.com/jinzhu/gorm
+$ go get github.com/dgrijalva/jwt-go
+$ go get github.com/joho/godotenv
+$ go get github.com/lib/pq
+$ go run main.go
+```
+
+
+### Development
+
+To test the current service, just run this command in command line
+
+```sh
+$ go run main.go
+```
+And it will shows the results of test.
+
+As there are three main service in this project, these are the details of each service
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"./app"
+	"./controllers"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+	router.Use(app.JwtAuthentication)
+	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
+	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
+	router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
+	router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET") //attach JWT auth middleware
+
+	port := os.Getenv("PORT") //Get port from .env file, we did not specify any port so this should return an empty string when tested locally
+	if port == "" {
+		port = "8000" //localhost
+	}
+
+	fmt.Println(port)
+
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
+	if err != nil {
+		fmt.Print(err)
+	}
+}
